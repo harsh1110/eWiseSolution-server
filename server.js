5ef953dc5ef77bc7fc91e5a612bae831428934eb
Gohil Harsh <66783334+harsh1110@users.noreply.github.com>
2022-03-15 15:00:04 +0530

    fghj

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 require("dotenv").config()
-const dbConnect = require("./config/dbConn")
+const dbConnect = require("./Config/dbConn.js")
 const cors = require('cors')
 
 const app = express()
